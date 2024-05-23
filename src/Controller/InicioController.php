145e9cace4d2442b49ba74666c07c1cf0be0281f
Maruve8 <mv.contrerasbellido@gmail.com>
2024-05-24 00:35:40 +0200

    rol administrador y todas sus vistas

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -4,12 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class InicioController extends AbstractController
 {
     #[Route('/inicio', name: 'app_inicio')]
-    public function index()
+    public function index(): Response
     {
-        return $this->render('inicio/index.html.twig');
+        $user = $this->getUser();
+
+        if ($this->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
+        return $this->render('inicio/index.html.twig', [
+            'user' => $user,
+        ]);
     }
 }
+
+
---
+++ src/Controller/PostLoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class PostLoginController extends AbstractController
+{
+    #[Route('/post_login', name: 'post_login')]
+    public function index(AuthorizationCheckerInterface $authorizationChecker)
+    {
+        if ($authorizationChecker->isGranted('ROLE_ADMIN')) {
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
+        return $this->redirectToRoute('app_inicio');
+    }
+}
